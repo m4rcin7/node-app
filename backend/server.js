8ae13285f0617ffba55953078e3a369fde600a29
m4rcin7 <mrku1337@gmail.com>
2025-06-26 14:03:07 +0200

    commit

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,14 @@ app.post("/api/products", async (req, res) => {
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
 
-  console.log("id:", id);
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 app.listen(5000, () => {
