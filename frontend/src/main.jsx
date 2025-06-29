092db37f073b8ace0de188bf8f0ceae93105f534
m4rcin7 <mrku1337@gmail.com>
2025-06-29 16:42:05 +0200

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
