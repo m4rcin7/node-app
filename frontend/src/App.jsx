877c6d5dafb5c6a3f3b7330835d25bc65fce0948
m4rcin7 <mrku1337@gmail.com>
2025-07-01 10:08:52 +0200

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
-import HomePage from "./pages/HomePage";
+
 import CreateProduct from "./pages/CreateProduct";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Box
-      minH={"100vh"}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateProduct />} />
