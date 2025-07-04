e4860241805d8efea2a7714f0caceba7e9893f49
m4rcin7 <mrku1337@gmail.com>
2025-07-04 17:00:55 +0200

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,20 @@
-// import { Box, useColorModeValue } from "@chakra-ui/react";
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-// import CreateProduct from "./pages/CreateProduct";
-// import HomePage from "./pages/HomePage";
-// import Navbar from "./components/Navbar";
-
-// function App() {
-//   return (
-//     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/create" element={<CreateProduct />} />
-//       </Routes>
-//     </Box>
-//   );
-// }
-
-// export default App;
-
-
-import { Box,  } from "@chakra-ui/react";
+import CreateProduct from "./pages/CreateProduct";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  return <Box bg={bg} minH="100vh">Hello</Box>;
+  return (
+    <>
+      {" "}
+      <Routes>
+        <Navbar />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateProduct />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,11 @@
 export default function Navbar() {
   return (
-    <Box
-      as="nav"
-      bg="teal.500"
-      color="white"
-      p={4}
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Box fontSize="xl" fontWeight="bold">
-        MyApp
-      </Box>
-      <Box>
-        <Button colorScheme="teal" variant="outline" mr={4}>
-          Home
-        </Button>
-        <Button colorScheme="teal" variant="outline">
-          Create Product
-        </Button>
-      </Box>
-    </Box>
+    <header>
+      <div>MyApp</div>
+      <div>
+        <a link="/">Home</a>
+        <a link="/create">Create Product</a>
+      </div>
+    </header>
   );
 }
