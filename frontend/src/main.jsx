bd3ee3974a1ba58a0cdf0ac8d7078e6dbc2cf986
m4rcin7 <mrku1337@gmail.com>
2025-07-05 20:43:17 +0200

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import CreateProduct from "./pages/CreateProduct";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateProduct />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
