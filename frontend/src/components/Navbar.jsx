edf52de26ca9ac196333188fbc7a2e6f3017380c
m4rcin7 <mrku1337@gmail.com>
2025-07-06 15:47:53 +0200

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,43 @@
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header>
-      <div>MyApp</div>
-      <nav>
-        <NavLink to="/" end>
-          Home
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-light">
+      <div className="container-fluid">
+        {/* Logo */}
+        <NavLink className="navbar-brand" to="/">
+          MyStore
         </NavLink>
-        <NavLink to="/create">Create Product</NavLink>
-      </nav>
-    </header>
+
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navigation Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link" end>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/create" className="nav-link">
+                Create Product
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateProduct from "./pages/CreateProduct";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
