556181296ee85a36a227997b4afd374e90c99cf4
Marcin Kopatys <mrku1337@gmail.com>
2025-07-10 19:30:58 +0200

    commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,53 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
+
 export default function HomePage() {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("products", products);
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="container py-5">
+      <div className="text-center mb-5">
+        <h2
+          className="fw-bold"
+          style={{
+            background: "linear-gradient(to right, #06b6d4, #3b82f6)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          Current Products 🚀
+        </h2>
+      </div>
+
+      <div className="row g-4">
+        {products.map((product) => (
+          <div key={product._id} className="col-12 col-md-6 col-lg-4">
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+
+      {products.length === 0 && (
+        <div className="text-center mt-5">
+          <h5 className="text-muted fw-bold">
+            No products found 😢{" "}
+            <Link
+              to="/create"
+              className="text-primary text-decoration-underline"
+            >
+              Create a product
+            </Link>
+          </h5>
+        </div>
+      )}
     </div>
   );
 }
